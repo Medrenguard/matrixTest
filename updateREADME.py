ffe6de2d5a378042a59c7f09e584969f7d069612
Medrenguard <0892364395@mail.ru>
2020-01-11 03:38:42 +0400

    reverse project

---
+++ updateREADME.py
@@ -0,0 +1,6 @@
+﻿## -*- coding: utf-8 -*-
+f = open('dump.txt')
+f2 = open('README.md', 'w')
+f2.write(f.read())
+f.close()
+f2.close()
---
+++ valueGrabber.py
@@ -0,0 +1,17 @@
+﻿## -*- coding: utf-8 -*-
+import re
+print("Результаты вступительных экзаменов представлены в виде списка из N строк, в каждой строке которого записаны фамилия студента и отметки по каждому из h экзаменов. Определить количество абитуриентов, сдавших вступительные экзамены на \"отлично\". \n")
+
+defFile = 'dump.txt'
+
+achieversCount = 0
+
+def detectAchiever(st):
+    return re.search('( 5){3}$', st)
+
+with open(defFile) as f:
+    arr = f.readlines()
+    for i in range(len(arr)):
+        if detectAchiever(arr[i]):
+            achieversCount += 1
+    print('Количество абитуриентов, сдавших на "отлично" -', achieversCount)
