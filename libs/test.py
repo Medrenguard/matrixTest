e40e06686e0f18331495121a97eacee5e87a518f
Medrenguard <0892364395@mail.ru>
2020-01-11 10:16:57 +0400

    Upd text for unsuccessful tests

--- libs/test.py
+++ libs/test.py
@@ -6,8 +6,8 @@ def maintest(scheme, arr):
         if re.search('([А-Яа-я]*)( \d){3}$', re.sub("^\s+|\n|\r|\s+$", '', arr[i])):
             v = re.search('([А-Яа-я]*)( \d){3}$', re.sub("^\s+|\n|\r|\s+$", '', arr[i])).group(0)
         else:
-            return([0, 'Not all lines match the pattern, stop the program!'])
+            return([0, 'Not all lines match the pattern!'])
         b = re.sub("^\s+|\n|\r|\s+$", '', arr[i])
         if v != b:
-            return([0, 'Not all lines match the pattern, stop the program!'])
+            return([0, 'Not all lines match the pattern!'])
     return([1, 'Check input data successfully finished!'])
